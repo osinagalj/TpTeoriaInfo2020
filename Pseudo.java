3bdeaa1f3fd1cf001a56ed4b25fef8ce85f5a5b8
Mura-kmi <agustinmurakami@gmail.com>
2020-05-29 09:16:43 -0300

    todos los pseudo, hay que arreglarlos <3

--- Pseudo.java
+++ Pseudo.java
@@ -34,15 +34,37 @@ public class Pseudo {
         return rta;
     }
 
-    public void arboldeHuffman(){
-
+    public arbol arbolDeHuffman(probabilidades, imagen){
+        probabilidades[] = ordenar(probabilidades[]);    //Ordenamos las probabilidades de menor a mayor
+        while (probabilidades[].size > 1){              //Mientras tengamos minimos 2 simbolos
+            new arbol(probabilidad[0],probabilidad[1]);
+            retirarProbabilidades;                      //Creamos un nodo con ambas probabilidades sumadas
+            instertarArbol(probabilidades[], arbol)     //lo incertamos en las probabilidades
+        }
+        recorrerArbol(Hash(Simbolo,Secuencia));     //Recorremos generando secuencias para las hojas
+        obtenerSecuencia(Imagen);                   //Recorremos cada pixel, codificando una secuencia
     }
 
-    public void comprimir(){
-
+    public archivo comprimir(secuencia){
+        bytes1[] = codificarAByte(secuencia.Longitud);          //Escribo la longitud
+        bytes2[] = codificarAByte(secuencia.cerosAdicionales);  //Escribo la cantidad de 0
+        bytes3[] = codificarAByte(secuencia);                   //Escribo la secuencia
+        archivo = write(bytes1[], bytes2[], bytes3[]);          //Guardo t0do en el archivo
     }
 
-    public void descomprimir(){
-
+    public void descomprimir(archivo){
+        Bytes[] = decodificarSecuencia(archivo);    //Convertimos la lista de bytes en una secuencia de char
+        arbol = armarArbol(header(Bytes[]));        //Replicamos el arbol de Huffman
+        for(secuencia(Bytes[])){        //Recorremos toda la lista restante
+            if(0)                           //Si viene un 0 avanza en el arbol por izq
+                arbol = arbol.izq;
+            else                            //Si no, por derecha
+                arbol = arbol.der;
+            if(arbol = hoja) {              //Si una es hoja
+                añadirImagen(arbol);            //Añadimos el color a la imagen
+                arbol = Raiz;                   //Volvemos a la raiz
+            }
+        }
     }
+
 }
